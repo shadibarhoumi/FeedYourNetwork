b5018dcbd82a62835eeab9acf3c9eef51cfcaa51
Shadi Barhoumi <shadibarhoumi@gmail.com>
2013-06-08 11:40:59 -0700

    notifications working

--- FeedYourNetwork.js
+++ FeedYourNetwork.js
@@ -1,19 +1,42 @@
 if (Meteor.isClient) {
 
+  // contacts
   Template.contacts.contact = function() {
     return Contacts.find({}).fetch();
   };
 
   Template.contacts.events({
-   'click .submit': function(e) {
-      var name = $('.name').val();
-      var email = $('.email').val();
+   'click .input-submit': function(e) {
+      var name = $('.input-name').val();
+      var email = $('.input-email').val();
       Contacts.insert({
         name: name,
         email: email
       });
-    } 
+    }, 
+    'click .name, click .avatar': function(e) {
+      var contactId = $(e.target).closest('li').attr('id');
+      var contact = Contacts.findOne(contactId);
+      var flag = contact.flagged ? false : true;
+      Contacts.update(contactId, {$set: {flagged: flag}});
+      contact = Contacts.findOne(contactId);
+      if (contact.flagged) {
+        Notifications.insert({
+          message: "You have flagged " + contact.name
+        });
+      } else {
+        Notifications.insert({
+          message: "You have unflagged " + contact.name
+        });
+      }
+    }
   });
+
+
+  // notifications
+  Template.notifications.notification = function() {
+    return Notifications.find({}).fetch();
+  };
 
 }
 
@@ -37,6 +60,7 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+
     // code to run on server at startup
   });
 }
