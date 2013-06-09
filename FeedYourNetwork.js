a3c7b9f32b3980b2f2c22b764b8ace58064ed441
Chris Gervang <gervangster@live.com>
2013-06-09 08:09:41 -0700

    try

--- FeedYourNetwork.js
+++ FeedYourNetwork.js
@@ -173,26 +173,3 @@ if (Meteor.isClient) {
 
 
 //////////////////////////////////////////////////////// SERVER ////////////////////////////////////////////////////////
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    Contacts.allow({
-      insert: function(userId, doc) {
-        return userId === doc.userId;
-      },
-      update: function(userId, doc) {
-        return userId === doc.userId;
-      }
-    });
-
-    Notifications.allow({
-      insert: function() {
-        return true;
-      },
-      remove: function() {
-        return true;
-      }
-    });
-
-  });
-}
--- server/main.js
+++ server/main.js
@@ -1,12 +1,5 @@
 Meteor.startup(function () {
 
-  Meteor.methods({
-    'updateLinkedin' : function(updatedLinkedinList) {
-      Contacts.update({userId:this.userId}, {$set: {linkedin:updatedLinkedinList}});
-    }
-  });
-  Contacts.remove({});
-
   Contacts.allow({
     insert: function(userId, doc) {
       return userId === doc.userId;
